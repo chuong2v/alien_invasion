d2692f64e6d8dc661ba80e57b8f5f8a3d4556a73
chuong vo <chuong2v@gmail.com>
2018-10-08 22:31:58 +0700

    movement: refactoring

--- settings.py
+++ settings.py
@@ -6,3 +6,10 @@ class Settings():
     self.screen_width = 1200
     self.screen_height = 600
     self.bg_color = (80, 80, 230)
+    self.ship_speed_factor = 1.5
+
+    # Bullet settings
+    self.bullet_speed_factor = 1
+    self.bullet_width = 3
+    self.bullet_height = 15
+    self.bullet_color = 60, 60,
--- ship.py
+++ ship.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Ship():
-  def __init__(self, screen):
+  def __init__(self, ai_settings, screen):
     """Initialize the ship and set its starting position."""
     self.screen = screen
     # Load the ship image and get its rect.
@@ -13,6 +13,9 @@ class Ship():
     self.rect.bottom = self.screen_rect.bottom
     self.moving_right = False
     self.moving_left = False
+    self.ai_settings = ai_settings
+    # Store a decimal value for the ship's center.
+    self.center = float(self.rect.centerx)
 
   def blitme(self):
     """Draw the ship at its current location."""
@@ -20,7 +23,10 @@ class Ship():
 
   def update(self):
     """Update the ship's position based on the movement flag."""
-    if self.moving_right:
-      self.rect.centerx += 1
-    elif self.moving_left:
-      self.rect.centerx -= 1
+    # Update the ship's center value, not the rect.
+    if self.moving_right and self.rect.right < self.screen_rect.right:
+      self.center += self.ai_settings.ship_speed_factor
+    if self.moving_left and self.rect.left > 0:
+      self.center -= self.ai_settings.ship_speed_factor
+    # Update rect object from self.center.
+    self.rect.centerx = self.center
